7e306ce042af4aaf1fff1a083552aec5529fcbf7
Joel Angel <joelbaraka4325@gmail.com>
2023-04-05 13:39:19 +0300

    added all the files and packages necessary for this projet

---
+++ screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text className="font-bold, text-center">
+        Coding my mind to be the best coder ever
+      </Text>
+    </View>
+  );
+};
+
+export default HomeScreen;

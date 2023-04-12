88569c8c81c8eebdc4e9a39b3609bf83068c14d7
Joel Angel <joelbaraka4325@gmail.com>
2023-04-12 23:37:30 +0300

    header section complete

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,23 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+import React, {useLayoutEffect} from 'react';
+
+import {useNavigation} from '@react-navigation/native';
+
 const HomeScreen = () => {
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  });
   return (
-    <View>
-      <Text className="font-bold, text-center">
-        Coding my mind to be the best coder ever
-      </Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text className="font-bold, text-center">
+          Coding my mind to be the best coder ever
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 };
 

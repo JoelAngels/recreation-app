95946193d577075584fbaf990b9f6d36b6c1c7bc
Joel Angel <joelbaraka4325@gmail.com>
2023-04-13 16:25:25 +0300

    initialied the logo plus the header

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,8 @@
-import {View, Text, SafeAreaView} from 'react-native';
+import {View, Text, SafeAreaView, Image} from 'react-native';
 import React, {useLayoutEffect} from 'react';
 
 import {useNavigation} from '@react-navigation/native';
+import {avatar} from '../assets';
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -11,11 +12,22 @@ const HomeScreen = () => {
     });
   });
   return (
-    <SafeAreaView>
-      <View>
-        <Text className="font-bold, text-center">
-          Coding my mind to be the best coder ever
-        </Text>
+    <SafeAreaView className="bg-white flex-1 relative">
+      {/* {First Section} */}
+
+      <View className="flex-row mt-8 items-center space-x-2 px-6 justify-between">
+        <View className="w-16 h-16 bg-black rounded-full items-center justify-center">
+          <Image
+            source={avatar}
+            className="w-full h-full rounded-md object-cover"
+          />
+        </View>
+        <View className="w-16 h-16 rounded-md items-center justify-center">
+          <Image
+            source={avatar}
+            className="w-full h-full rounded-md object-cover"
+          />
+        </View>
       </View>
     </SafeAreaView>
   );
